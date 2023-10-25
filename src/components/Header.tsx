836bb9e7d221dc130562f33d133d7bd0a0889f33
Weiran Wu <weiranwu@microsoft.com>
2023-10-25 10:36:49 +0800

    Update logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -50,7 +50,7 @@ const Header = () => {
           <PanelLeftExpand24Regular />
         </button>
         <div className="flex links">
-          <Image src={logo} height={100} title={t("m1knight logo")} />
+          <Image src={logo} height={36} title={t("m1knight logo")} />
           <div className="select-lang">
             <Globe24Regular />
             <Select
